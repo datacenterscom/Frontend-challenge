7f45c99229b28865ebf24f5e67f75cadd08a3e64
Engels Prado <engelsprado7@gmail.com>
2025-09-10 11:00:13 -0600

    Add list-characters.service.ts to fetch characters from Rick & Morty API

---
+++ src/services/list-characters.service.ts
@@ -0,0 +1,10 @@
+import { executeQuery } from "@/lib/graphql/client";
+
+
+/**
+ * Fetch all characters from the Rick & Morty API
+ * @returns Promise with characters data
+ */
+export function getCharacters() {
+  // return executeQuery<GraphQLResponseInterface>(YOUR_QUERY);
+}
